979892c7a9200dd2c0c0b03f23512b1cc229edba
Vijayendra2707 <ranevijayendra@gmail.com>
2026-08-04 17:03:26 +0530

    Frontend React Done

--- frontend/resume-frontend/src/Login.js
+++ frontend/resume-frontend/src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 function Login() {
   const [email, setEmail] = useState("");
